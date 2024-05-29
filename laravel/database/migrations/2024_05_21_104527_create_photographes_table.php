d3e039edcbd1c904e80bf8232da91385fc38e7ec
franck-gif <franclaintomayo45@gmail.com>
2024-05-29 01:54:25 +0100

    ajout du Type user pour nous permettre de gerer les autorisations : soit c'est un Administrateur ou un Photographe

--- laravel/database/migrations/2024_05_21_104527_create_photographes_table.php
+++ laravel/database/migrations/2024_05_21_104527_create_photographes_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('photographes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->unique()->constrained("users");
             $table->string("nom");
             $table->string("ville");
             $table->string("pays");
